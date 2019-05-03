3ed31408a5e6783211194525f05fa096d1be0cbc
janessabinocode <janessabino@gmail.com>
2019-05-03 16:59:50 -0300

    my first commit

---
+++ index.cs
@@ -0,0 +1,37 @@
+using System;
+namespace curso {
+    class Program{
+    static void main (string[] args){
+        int x = int.Parse(Console.ReadLine());
+        string dia;
+        if (x==1){
+            dia = "Domingo";
+        }
+        else if (x==2){
+            dia = "Segunda-feira";
+        }
+        else if (x==3){
+            dia = "Terça-Feira";
+        }
+        else if (x==4){
+            dia = "Quarta-Feira";
+        }
+        else if (x==5){
+            dia = "Quinta-feira";
+        }
+        else if (x==6){
+            dia = "Sexta-Feira";
+        }
+        else if (x==7){
+            dia = "Sábado";
+        }
+        else{
+            dia = "Valor inválido!";
+        }
+
+        Console.WriteLine("Dia da semana: " + dia);
+        Console.ReadLine();
+
+        }
+    }
+}
